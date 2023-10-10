f130138455e394c33ee846a1b247d9ccff92aa1b
Ekene <ekeneezeifeomasam27@gmail.com>
2023-10-10 02:33:29 +0100

    initalized next auth and created the user schema in prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import React from "react";
-import { RegisterLink, LoginLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import React from 'react'
 
 export default function page() {
-  const {getUser} = getKindeServerSession()
-  const user = getUser()
   return (
-    <div>
-      <pre>
-        {user && JSON.stringify(user)}
-      </pre>
-      <LoginLink>Sign in</LoginLink>
-
-      <RegisterLink>Sign up</RegisterLink>
-    </div>
-  );
+    <div>page</div>
+  )
 }
